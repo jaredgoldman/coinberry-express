ed76b7f1082a85354a189af94374fc768b38405d
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-10-07 17:08:28 -0400

    add balance response logging

--- routes/index.js
+++ routes/index.js
@@ -79,6 +79,7 @@ router.get('/balance', async (req, res) => {
       console.log('>>> data', data);
       res.send({ data: data, success: true });
     } else {
+      console.log('>>> response', response);
       res.send({ data: 'error', success: false });
     }
   } catch (error) {
