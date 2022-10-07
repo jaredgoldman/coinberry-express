9a35877eb35c49ed247cdf28b634b72749054138
jaredgoldman <jaredgoldmandrums@gmail.com>
2022-10-07 16:19:55 -0400

    add async to register/activate call

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
-var express = require('express')
-var router = express.Router()
-const axios = require('axios')
-const convert = require('xml-js')
-var database = require('../database')
+var express = require('express');
+var router = express.Router();
+const axios = require('axios');
+const convert = require('xml-js');
+var database = require('../database');
 
 const balanceBody = {
   corrId: 'testCorrId005',
   userId: 'wsdemo8153',
   password: 'kExCbg8X25N9dujY',
   token: '0xf52ad35ce653d661b4d6d290feeb4009',
-}
+};
 
 const registerBody = {
   corrId: 'testCorrId001',
@@ -32,7 +32,7 @@ const registerBody = {
   IDType: 'DLNo',
   IDNumber: 'K12341234512345',
   address2: '',
-}
+};
 
 const activateBody = {
   corrId: 'testCorrId003',
@@ -41,17 +41,17 @@ const activateBody = {
   token: '0xf52ad35ce653d661b4d6d290feeb4009',
   loadAmount: '1',
   txnDesc: 'Load from Coinberry',
-}
+};
 
 router.get('/test', (req, res) => {
-  res.send('test')
-})
+  res.send('test');
+});
 
 router.post('/balance', async (req, res, next) => {
   try {
-    const { user } = req.query
+    const { user } = req.query;
     if (!database[user].balance) {
-      res.send({ data: 'user not found', success: false })
+      res.send({ data: 'user not found', success: false });
     }
     const response = await axios.post(
       'https://ws2.trucash.com:452/cardserviceV2.asmx/Balance',
@@ -61,28 +61,28 @@ router.post('/balance', async (req, res, next) => {
           'content-type': 'application/x-www-form-urlencoded',
         },
       }
-    )
+    );
 
     if (response.status === 200) {
-      const xml = response.data
-      const json = JSON.parse(convert.xml2json(xml, {}))
-      const text = json.elements[0].elements[1].elements[0].text
-      const data = text.split(',')[2]
-      console.log('>>> data', data)
-      res.send({ data: data, success: true })
+      const xml = response.data;
+      const json = JSON.parse(convert.xml2json(xml, {}));
+      const text = json.elements[0].elements[1].elements[0].text;
+      const data = text.split(',')[2];
+      console.log('>>> data', data);
+      res.send({ data: data, success: true });
     } else {
-      res.send({ data: 'error', success: false })
+      res.send({ data: 'error', success: false });
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
 router.post('/activate', async (req, res, next) => {
   try {
-    const { user } = req.body
+    const { user } = req.body;
     if (!database[user].activation) {
-      res.send({ data: 'user not found', success: false })
+      res.send({ data: 'user not found', success: false });
     }
     const response = await axios.post(
       'https://ws2.trucash.com:452/cardserviceV2.asmx/Activate',
@@ -92,25 +92,26 @@ router.post('/activate', async (req, res, next) => {
           'content-type': 'application/x-www-form-urlencoded',
         },
       }
-    )
+    );
 
     if (response.status === 200) {
-      const xml = response.data
-      const json = convert.xml2json(xml, {})
-      res.send({ data: JSON.parse(json), success: true })
+      const xml = response.data;
+      const json = convert.xml2json(xml, {});
+      res.send({ data: JSON.parse(json), success: true });
     } else {
-      res.send({ data: 'error', success: false })
+      res.send({ data: 'error', success: false });
     }
   } catch (error) {
-    console.log(error.response.data)
+    console.log(error.response.data);
   }
-})
+});
 
-router.post('register/activate', (req, res) => {
-  const { user } = req.body
+router.post('register/activate', async (req, res) => {
+  const { user } = req.body;
   if (!database[user].registration) {
-    res.send({ data: 'user registration details not found', success: false })
+    res.send({ data: 'user registration details not found', success: false });
   }
+
   const registerResponse = await axios.post(
     'https://ws2.trucash.com:452/cardserviceV2.asmx/Register',
     database[user].register,
@@ -119,11 +120,11 @@ router.post('register/activate', (req, res) => {
         'content-type': 'application/x-www-form-urlencoded',
       },
     }
-  )
+  );
 
   if (registerResponse.status === 200) {
     if (!database[user].activation) {
-      res.send({ data: 'user activation details not found', success: false })
+      res.send({ data: 'user activation details not found', success: false });
     }
     const activationResponse = await axios.post(
       'https://ws2.trucash.com:452/cardserviceV2.asmx/Activate',
@@ -133,17 +134,25 @@ router.post('register/activate', (req, res) => {
           'content-type': 'application/x-www-form-urlencoded',
         },
       }
-    )
+    );
     if (activationResponse.status === 200) {
-      const xml = activationResponse.data
-      const json = convert.xml2json(xml, {})
-      res.send({ data: JSON.parse(json), success: true })
+      const xml = activationResponse.data;
+      const json = convert.xml2json(xml, {});
+      res.send({ data: JSON.parse(json), success: true });
     } else {
-      res.send({ data: 'activation failed, could not activate card', success: false })
+      res.send({
+        data: 'activation failed, could not activate card',
+        success: false,
+      });
     }
   } else {
-    res.send({ data: 'registration failed, could not register user', success: false })
+    res.send({
+      data: 'registration failed, could not register user',
+      success: false,
+    });
   }
-})
+});
 
-module.exports = router
+// router.post('/load', async);
+
+module.exports = router;
